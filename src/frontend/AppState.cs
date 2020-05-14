caddb57d72f02722ae80103383e6db601b6a13b5
Christopher Maneu <christopher@maneu.net>
2020-05-14 16:25:29 +0200

    Add exam welcome page

--- src/frontend/AppState.cs
+++ src/frontend/AppState.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorQuiz.Model;
 
 namespace BlazorQuiz.FrontEnd
 {
@@ -10,7 +11,11 @@ namespace BlazorQuiz.FrontEnd
         public bool IsExamLoaded { get; set; } = false;
 
         public string CurrentExamTitle { get; set; }
-        
+
+        public string CurrentExamLogo { get; set; }
+
+        public Candidate CurrentCandidate { get; set; }
+
         public event Action OnChange;
 
         public void NotifyStateChanged() => OnChange?.Invoke();
