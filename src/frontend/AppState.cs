572b88f9ccf6ca4b66773d79f2d5318a2ca12fdf
Christopher Maneu <christopher@maneu.net>
2020-05-14 15:22:25 +0200

    Add base layout and App State

---
+++ src/frontend/AppState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorQuiz.FrontEnd
+{
+    public class AppState
+    {
+        public bool IsExamLoaded { get; set; } = false;
+
+        public string CurrentExamTitle { get; set; }
+        
+        public event Action OnChange;
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+
+    }
+}
