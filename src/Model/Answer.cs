83d44b4815e24d993833ef2c7cfe04c3e8587cae
Christopher Maneu <christopher@maneu.net>
2020-05-14 14:41:36 +0200

    Add functions to save the results in CosmosDB

--- src/Model/Answer.cs
+++ src/Model/Answer.cs
@@ -1,11 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace BlazorQuiz.Model
 {
     public class Answer
     {
+
+        public string AnswerId => CandidateId + Question.Id;
+
+        [JsonPropertyName("candidateId")]
+        public string CandidateId { get; set; }
+        
+        [JsonPropertyName("question")]
         public Question Question { get; set; }
+        
+        [JsonPropertyName("choices")]
         public List<AnswerChoice> Choices { get; set; }
-        public bool IsCorrect => Choices.TrueForAll(a => a.IsCorrect);
+
+        [JsonPropertyName("answeredAt")]
+        public DateTime AnsweredAt { get; set; }
+
+        [JsonIgnore]
+        public bool IsCorrect
+        {
+            get
+            {
+                return Choices != null && Choices.TrueForAll(a => a.IsCorrect);
+            }
+        }
     }
 }
--- src/Model/AnswerChoice.cs
+++ src/Model/AnswerChoice.cs
@@ -5,5 +5,16 @@
         public bool IsSelected { get; set; }
 
         public bool IsCorrect => IsSelected == IsCorrectChoice;
+
+        public static AnswerChoice FromChoice(Choice choice)
+        {
+            var answerChoice = new AnswerChoice();
+            answerChoice.Text = choice.Text;
+            answerChoice.IsCorrectChoice = choice.IsCorrectChoice;
+            answerChoice.ImageContents = choice.ImageContents;
+            answerChoice.Rationale = choice.Rationale;
+
+            return answerChoice;
+        }
     }
 }
