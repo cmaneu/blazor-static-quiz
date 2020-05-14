f645df0e3f622971b7c6d9455f8c330cc987b70d
Christopher Maneu <christopher@maneu.net>
2020-05-14 18:54:13 +0200

    Add Exam page and a base exam results page

--- src/Model/AnswerChoice.cs
+++ src/Model/AnswerChoice.cs
@@ -1,14 +1,20 @@
-﻿namespace BlazorQuiz.Model
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorQuiz.Model
 {
     public class AnswerChoice : Choice
     {
+        [JsonIgnore]
+        public int Id { get; set; }
+
         public bool IsSelected { get; set; }
 
         public bool IsCorrect => IsSelected == IsCorrectChoice;
 
-        public static AnswerChoice FromChoice(Choice choice)
+        public static AnswerChoice FromChoice(Choice choice, int id)
         {
             var answerChoice = new AnswerChoice();
+            answerChoice.Id = id;
             answerChoice.Text = choice.Text;
             answerChoice.IsCorrectChoice = choice.IsCorrectChoice;
             answerChoice.ImageContents = choice.ImageContents;
--- src/frontend/Services/ExamService.cs
+++ src/frontend/Services/ExamService.cs
@@ -1,11 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using BlazorQuiz.Model;
 
 namespace BlazorQuiz.FrontEnd.Services
 {
     public class ExamService
     {
+        private HttpClient _httpClient;
+        private ClientAppSettings _appSettings;
+        private AppState _appState;
+
+        public AnswerSheet AnswerSheet { get; protected set; }
+
+        public Exam CurrentExam { get; set; }
+
+        public Question CurrentQuestion { get; set; }
+
+        public ExamService(HttpClient httpClient, ClientAppSettings appSettings, AppState appState)
+        {
+            _httpClient = httpClient;
+            _appSettings = appSettings;
+            _appState = appState;
+        }
+
+        public async Task LoadExam(string examId)
+        {
+            CurrentExam = await _httpClient.GetFromJsonAsync<Exam>($"exam-data/{examId}.json");
+            _appState.CurrentExamTitle = CurrentExam.ExamTitle;
+            _appState.CurrentExamLogo = CurrentExam.ExamImage;
+            _appState.NotifyStateChanged();
+            AnswerSheet = new AnswerSheet();
+        }
+
+        public async Task<List<AnswerChoice>> LoadNextQuestionChoices()
+        {
+            CurrentQuestion = CurrentQuestion == null 
+                ? CurrentExam.Questions.FirstOrDefault() 
+                : CurrentExam.Questions.ElementAtOrDefault(CurrentExam.Questions.IndexOf(CurrentQuestion) + 1);
+
+            return CurrentQuestion?.GetAnswerChoices().ToList();
+        }
+
+        public async void SubmitQuestionAnswer(Answer answer)
+        {
+            if (AnswerSheet.Candidate == null)
+            {
+                AnswerSheet.Candidate = _appState.CurrentCandidate;
+            }
+
+            if (AnswerSheet.StartedAt == DateTime.MinValue)
+            {
+                AnswerSheet.StartedAt = answer.AnsweredAt;
+            }
+            
+            AnswerSheet.Answers.Add(answer);
+            Task.Run(async () =>
+            {
+                await _httpClient.PostAsJsonAsync($"{_appSettings.ApiBaseUrl}answer/{_appState.CurrentCandidate.Email}",
+                    answer);
+            } );
+        }
+
+        public async Task EndExam()
+        {
+            AnswerSheet.EndedAt = DateTime.UtcNow;
+            AnswerSheet.ComputeScore();
+        }
     }
 }
