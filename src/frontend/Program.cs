57520fb9b9cdb74131bd92d90fc8b00815d26650
Christopher Maneu <christopher@maneu.net>
2020-06-09 20:08:46 +0200

    Add localization

--- src/frontend/Program.cs
+++ src/frontend/Program.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using BlazorQuiz.FrontEnd.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+using Microsoft.JSInterop;
 
 namespace BlazorQuiz.FrontEnd
 {
@@ -19,11 +21,23 @@ namespace BlazorQuiz.FrontEnd
             builder.Services.AddSingleton<ClientAppSettings>();
             builder.Services.AddSingleton<AppState>();
             builder.Services.AddSingleton<ExamService>();
+            builder.Services.AddLocalization();
+
 
             var host = builder.Build();
 
             ClientAppSettings appSettings =  host.Services.GetService(typeof(ClientAppSettings)) as ClientAppSettings;
             await appSettings.LoadAsync();
+
+
+            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+            var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+            if (result != null)
+            {
+                var culture = new CultureInfo(result);
+                CultureInfo.DefaultThreadCurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentUICulture = culture;
+            }
 
             await host.RunAsync();
         }
